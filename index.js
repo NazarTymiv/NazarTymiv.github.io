107b7c2cc2da5346cb4b7f0f7170760ada71d205
NazarTymiv <50737309+NazarTymiv@users.noreply.github.com>
2021-04-19 11:54:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const routes = require('./routes/routes')
 
+const PORT = process.evn.PORT || 3000;
+
 const app = express()
 const hbs = exphbs.create({
     defaultLayout: 'main',
@@ -14,3 +16,7 @@ app.use(routes)
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
+
+app.listen(PORT, ()  =>{
+    console.log("Server started successfully");
+})
