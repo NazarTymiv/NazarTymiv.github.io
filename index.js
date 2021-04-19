cb2c96789b05d578ee819c8d854cb8ac154931d9
NazarTymiv <50737309+NazarTymiv@users.noreply.github.com>
2021-04-19 11:37:32 +0300

    Delete localhost

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const routes = require('./routes/routes')
 
-const PORT = process.env.PORT || 5000
-
 const app = express()
 const hbs = exphbs.create({
     defaultLayout: 'main',
@@ -16,7 +14,3 @@ app.use(routes)
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
-
-app.listen(PORT, () => {
-    console.log('Server started succesfully')
-})
